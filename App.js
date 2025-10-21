a945586c2a052dadc0a6f2e54b195198b7766df5
mohammed10 <akhichkhouch2017@gmail.com>
2025-10-21 14:25:36 +0100

    fisrt commit

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar,Link } from 'expo-status-bar';
+import { StyleSheet, Text, ImageBackground,Pressable } from 'react-native';
+
+import imagg from "./assets/back.jpg"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  
+    <ImageBackground source ={imagg} style={styles.container} >
+      
+      <Text style={styles.Text} >BrewTimeCoffe
+      </Text>
+     
+      <Pressable
+        onPress={() => alert('Pressed!')}
+        style={({ pressed }) => [
+          styles.button,
+          pressed && styles.buttonPressed, 
+        ]}
+      >
+        <Text style={styles.buttonText}>Click Here</Text>
+        <Link href="\menu">to menu</Link>
+
+      </Pressable>
+
+
       <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
+
   );
 }
 
@@ -16,5 +36,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  Text :{
+    color: 'red',
+    backgroundColor:"rgba(209, 219, 213, 0.54)",
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  buttonText : {
+    color : 'red',
+    backgroundColor: 'yellow',
+    margin: 200,
+    width: 90,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  }
+
 });
